013bdf2a36df5c9dfb022413f443d3b1fd0e2c2f
Sneha harigharun <109866539+sneha2001@users.noreply.github.com>
2024-03-29 15:43:18 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,31 @@
+
+
+const btn = document.getElementById("btn");
+const img = document.getElementById("img");
+const sound = document.getElementById("sound");
+
+btn.addEventListener("click", getDogImage);
+
+function getDogImage() {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) => {
+      img.src = data.message;
+      sound.play();
+      sound.currentTime = 0;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+// loader
+$(window).on("load", () => {
+  setTimeout(removeLoader, 1700);
+});
+
+function removeLoader() {
+  $("#loadingDiv").fadeOut(500, () => {
+    $("#loadingDiv").remove();
+  });
+}
